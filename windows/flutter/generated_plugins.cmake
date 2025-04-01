0542ab0f2f12a4023a614c3e28554710ef43875f
simon <simon.louis638@gmail.com>
2025-04-01 10:38:50 +0200

    front/create-article

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,8 +3,13 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_selector_windows
   flutter_secure_storage_windows
+<<<<<<< HEAD
   geolocator_windows
+=======
+  permission_handler_windows
+>>>>>>> a84df33 (front/create-article)
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
